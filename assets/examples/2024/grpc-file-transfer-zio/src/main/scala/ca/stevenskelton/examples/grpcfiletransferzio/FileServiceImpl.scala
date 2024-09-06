5bc687eb2eb363309fa951d607be8eba37af55e9
Steven Skelton <stevenrskelton@gmail.com>
2024-09-06 10:15:12 -0400

    FileServiceImpl

--- assets/examples/2024/grpc-file-transfer-zio/src/main/scala/ca/stevenskelton/examples/grpcfiletransferzio/FileServiceImpl.scala
+++ assets/examples/2024/grpc-file-transfer-zio/src/main/scala/ca/stevenskelton/examples/grpcfiletransferzio/FileServiceImpl.scala
@@ -1,106 +1,106 @@
 package ca.stevenskelton.examples.grpcfiletransferzio
 
-import ca.stevenskelton.examples.grpcfiletransferzio.file_service.{GetFileRequest, FileChunk, SetFileResponse}
 import ca.stevenskelton.examples.grpcfiletransferzio.file_service.ZioFileService.FileService
-import io.grpc.StatusException
-import zio.{IO, stream}
-
+import ca.stevenskelton.examples.grpcfiletransferzio.file_service.{FileChunk, GetFileRequest, SetFileResponse}
 import com.google.protobuf.ByteString
 import io.grpc.StatusException
 import zio.nio.channels.*
 import zio.nio.file.*
 import zio.stream.{Stream, UStream, ZSink, ZStream}
-import zio.{Cause, Chunk, IO, Scope, ZIO}
+import zio.{Cause, Chunk, IO, Scope, ZIO, stream}
 
-import java.io.{File, IOException}
+import java.io.{File, FileNotFoundException, IOException}
 import java.nio.file.StandardOpenOption
 import scala.jdk.CollectionConverters.*
 
-class FileServiceImpl(filesDirectory: File, chunkSize: Int) extends FileService:
+class FileServiceImpl(filesDirectory: File, chunkSize: Int, maxFileSize: Long) extends FileService:
 
-  private case class SaveFileAccum(asynchronousFileChannel: AsynchronousFileChannel, file: File, totalSize: Long, offset: Long)
+  private def javaFile(unsafeFilename: String): File = File(s"${filesDirectory.getPath}/$unsafeFilename")
 
-  private def javaFile(unsafeFilename: String): File = 
-    new File(s"${filesDirectory.getPath}/$unsafeFilename")
-
-  override def getFile(request: GetFileRequest): Stream[StatusException, FileChunk] = {
+  override def getFile(request: GetFileRequest): Stream[StatusException, FileChunk] =
     val file = javaFile(request.filename)
-    ZStream.fromZIO(readFileSize(file)).flatMap {
-      fileSize =>
-        readFile(file).mapAccum(0)((sentBytes, byteString) =>
+    ZStream.fromZIO(readFileSize(file))
+      .flatMap: fileSize =>
+        readFile(file).mapAccum(0L)((sentBytes, byteString) =>
           val fileChunk = FileChunk.of(
             filename = file.getName,
             size = fileSize,
             offset = sentBytes,
             success = sentBytes + byteString.size == fileSize,
-            error = null,
             body = byteString,
           )
           (sentBytes + byteString.size, fileChunk)
         )
-      }
-      .catchAll {
-        ex => ZStream.fromZIO(ZIO.logCause("updateUserSocial", Cause.fail(ex)).flatMap(_ => ZIO.fail(StatusException(io.grpc.Status.fromThrowable(ex)))))
-      }
-  }
+      .catchAll: ex => 
+        ZStream.fromZIO(ZIO.logCause("updateUserSocial", Cause.fail(ex)).flatMap(_ => ZIO.fail(StatusException(io.grpc.Status.fromThrowable(ex)))))
 
   override def setFile(request: Stream[StatusException, FileChunk]): IO[StatusException, SetFileResponse] = {
+
+    case class SaveFileAccum(
+                              asynchronousFileChannel: AsynchronousFileChannel,
+                              file: File,
+                              totalSize: Long,
+                              offset: Long,
+                            )
+
     val sink = ZSink.foldLeftZIO[Scope, StatusException | IOException, FileChunk, Option[SaveFileAccum]](None)((saveFileAccum, fileChunk) =>
-      saveFileAccum match {
+      saveFileAccum match
+
+        case Some(SaveFileAccum(_, _, _, offset)) if fileChunk.offset != offset =>
+          ZIO.fail(IOException(s"Invalid chunk offset ${fileChunk.offset}, expected $offset"))
+
         case Some(SaveFileAccum(asyncFileChannel, file, totalSize, offset)) =>
-          if (fileChunk.offset != offset) {
-            ZIO.fail(IOException("Invalid chunk offset"))
-          } else {
-            val chunk = Chunk.from(fileChunk.body.asScala.map(Byte.unbox))
-            val saveFileAccum = SaveFileAccum(
-              asynchronousFileChannel = asyncFileChannel,
-              file = file,
-              totalSize = totalSize,
-              offset = offset + chunk.length,
-            )
+          val chunk = Chunk.from(fileChunk.body.asScala.map(Byte.unbox))
+          val saveFileAccum = SaveFileAccum(
+            asynchronousFileChannel = asyncFileChannel,
+            file = file,
+            totalSize = totalSize,
+            offset = offset + chunk.length,
+          )
+          if saveFileAccum.offset > totalSize then
+            ZIO.fail(IOException(s"Invalid chunk ${fileChunk.offset} exceeds total size $totalSize"))
+          else
             asyncFileChannel.writeChunk(chunk, offset).as(Some(saveFileAccum))
-          }
+
         case None =>
           val file = javaFile(fileChunk.filename)
           val path = Path.fromJava(file.toPath)
-          if (fileChunk.size == 0) {
-            Files.deleteIfExists(path).as(Some(SaveFileAccum(null, file, 0, 0)))
-          } else {
+          val chunk = Chunk.from(fileChunk.body.asScala.map(Byte.unbox))
+          if fileChunk.size > maxFileSize then
+            ZIO.fail(IOException(s"File too large, attempted ${fileChunk.size} bytes"))
+          else if fileChunk.size < chunk.length then
+            ZIO.fail(IOException(s"Invalid chunk ${chunk.length} exceeds total size ${fileChunk.size}"))
+          else
             AsynchronousFileChannel.open(
-              path,
-              StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE
-            ).flatMap {
-              channel =>
-                val chunk = Chunk.from(fileChunk.body.asScala.map(Byte.unbox))
+                path,
+                StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE
+              )
+              .flatMap: channel =>
                 val saveFileAccum = SaveFileAccum(channel, file, fileChunk.size, chunk.length)
                 channel.writeChunk(chunk, 0).as(Some(saveFileAccum))
-            }
-          }
-      }
     )
     val z = ZIO.scoped {
-      request.run(sink).flatMap {
+      request.run(sink).flatMap:
         case Some(SaveFileAccum(_, file, expectedSize, actualSize)) if expectedSize == actualSize =>
           val response = SetFileResponse.of(file.getName)
           ZIO.succeed(response)
         case _ =>
           ZIO.fail(StatusException(io.grpc.Status.NOT_FOUND))
-      }
     }
-    z.catchAll:
-      ex => ZIO.logCause("setFile", Cause.fail(ex)).flatMap(_ => ZIO.fail(StatusException(io.grpc.Status.fromThrowable(ex))))
+    z.catchAll: ex => 
+      ZIO.logCause("setFile", Cause.fail(ex)).flatMap(_ => ZIO.fail(StatusException(io.grpc.Status.fromThrowable(ex))))
   }
 
   private def readFileSize(file: File): IO[IOException, Long] =
     val path = Path.fromJava(file.toPath)
-    zio.nio.file.Files.exists(path)
-      .filterOrFail(_ == true)(java.io.FileNotFoundException(file.getName))
-      .flatMap:
-        _ => zio.nio.file.Files.size(path)
+    Files.exists(path)
+      .filterOrFail(_ == true)(FileNotFoundException(file.getName))
+      .flatMap: _ => 
+        Files.size(path)
 
   private def readFile(file: File): UStream[ByteString] =
-    ZStream.fromPath(file.toPath, chunkSize = chunkSize).chunks
-      .map:
-        chunk => ByteString.copyFrom(chunk.toArray)
-      .catchAll:
-        ex => ZStream.fromZIO(ZIO.logErrorCause(s"Error reading file ${file.getName}", Cause.fail(ex))).drain
+    ZStream.fromPath(file.toPath, chunkSize = chunkSize)
+      .chunks.map: chunk => 
+        ByteString.copyFrom(chunk.toArray)
+      .catchAll: ex => 
+        ZStream.fromZIO(ZIO.logErrorCause(s"Error reading file ${file.getName}", Cause.fail(ex))).drain
--- assets/examples/2024/grpc-file-transfer-zio/src/main/scala/ca/stevenskelton/examples/grpcfiletransferzio/Main.scala
+++ assets/examples/2024/grpc-file-transfer-zio/src/main/scala/ca/stevenskelton/examples/grpcfiletransferzio/Main.scala
@@ -2,16 +2,17 @@ package ca.stevenskelton.examples.grpcfiletransferzio
 
 import io.grpc.ServerBuilder
 import io.grpc.protobuf.services.ProtoReflectionService
-import scalapb.zio_grpc.{RequestContext, ServerLayer, ServiceList}
-import zio.{ExitCode, Schedule, URIO, ZIOAppDefault, ZLayer}
+import scalapb.zio_grpc.{ServerLayer, ServiceList}
+import zio.{ExitCode, URIO, ZIOAppDefault}
 
 import java.io.File
 
 object Main extends ZIOAppDefault:
 
   private val GRPCServerPort = 9000
-  private val FilesDirectory = new File("files")
-  private val ChunkSize = 65536
+  private val FilesDirectory = File("files")
+  private val ChunkSize = 65_536
+  private val MaxFileSize = 250 * 1_048_576L
   
   override def run: URIO[Any, ExitCode] =
     FilesDirectory.mkdir()
@@ -19,7 +20,7 @@ object Main extends ZIOAppDefault:
       grpcServer <- ServerLayer
         .fromServiceList(
           ServerBuilder.forPort(GRPCServerPort).addService(ProtoReflectionService.newInstance()),
-          ServiceList.add(FileServiceImpl(FilesDirectory, ChunkSize)),
+          ServiceList.add(FileServiceImpl(FilesDirectory, ChunkSize, MaxFileSize)),
         )
         .launch
     yield
