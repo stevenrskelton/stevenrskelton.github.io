cb19cc20fd4e255d26c06859c2aca9e10235b238
Steven Skelton <stevenrskelton@gmail.com>
2024-09-24 21:56:02 -0400

    clean up

--- examples/2024/flutter_realtime_database/generate_grpc_protos.sh
+++ examples/2024/flutter_realtime_database/generate_grpc_protos.sh
@@ -1,4 +1,5 @@
 #brew install protobuf
+#dart pub global activate protoc_plugin
 PATH="$PATH":"$HOME/.pub-cache/bin"
 mkdir -p lib/generated/protobuf;
 protoc --dart_out=grpc:lib/generated/protobuf -Iprotos protos/*.proto
